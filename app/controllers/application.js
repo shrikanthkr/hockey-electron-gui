146e1bc7f517cc98d37ca39da076e77e4cd8f19b
shrikanthkr <shrikanth_kr@yahoo.com>
2016-06-12 21:14:51 +0530

    build and clean, with electron

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,13 +1,15 @@
 import Ember from 'ember';
 
 const electron = requireNode('electron');
-const child_process = require('child_process');
+const child_process = requireNode('child_process');
+const {dialog} = requireNode('electron').remote;
 
 export default Ember.Controller.extend({
 	process: window.processNode,
 	electron: electron,
 	child_process: child_process,
-	execute: function(command, callback){
-		var output = this.child_process.exec(command,callback);
+	dialog: dialog,
+	execute: function(command, options, callback){
+		var output = this.child_process.exec(command,options, callback);
 	}
 });
--- app/controllers/index.js
+++ app/controllers/index.js
@@ -2,11 +2,33 @@ import Ember from 'ember';
 import ApplicationController from './application';
 
 export default  ApplicationController.extend({
+	path: "",
 	actions: {
 		save: function() {
-			this.execute("ls", (error, stdout, stderr) => {
+			let params = {};
+			params.cwd = this.get('path');
+			this.execute("./gradlew assembleRC", params, (error, stdout, stderr) => {
 				console.log(stdout);
+				console.log("ERROR:   " + error);
+				console.log("STDERR:"  + stderr);
 			});
+		},
+		clean: function() {
+			let params = {};
+			params.cwd = this.get('path');
+			this.execute("./gradlew clean", params, (error, stdout, stderr) => {
+				console.log(stdout);
+				console.log("ERROR:   " + error);
+				console.log("STDERR:"  + stderr);
+			});
+		},
+		open: function() {
+			this.get('dialog').showOpenDialog({properties: ['openDirectory']}, (paths) => {
+				if(paths){
+					this.set('path', paths[0]);
+					console.log(paths);
+				}
+			})
 		}
 	}
 });
