1c4cd417726de48ab1455b1c2c682330d3c73b47
Sudharshan <sudhar.ti@gmail.com>
2016-07-21 14:33:37 -0500

    Add console helper to check if console output is not null

--- app/components/build-type.js
+++ app/components/build-type.js
@@ -1,45 +1,46 @@
 import Ember from 'ember';
 import ElectronMixin from '../mixins/electron'
 export default Ember.Component.extend(ElectronMixin, {
-	success: false,
-	error: false,
-	is_loading: false,
-	prefix: 'assemble',
-	staticUrl: 'https://rink.hockeyapp.net/api/2/apps/upload',
-	pid: 0,
-	actions: {
-		build: function() {
-			this.set('is_loading', true);
-			let params = {};
-			params.cwd = this.get('path');
-			let command = this.get('type').command;
-			let buildTypes = [];
-			buildTypes.push(command);
-			let output = this.spawn("./gradlew",buildTypes, params);
-			this.set('pid', output.pid);
-			output.stdout.on('data', (data) => {
-				this.set('stdOut', data);
-			});
+  success: false,
+  error: false,
+  is_loading: false,
+  prefix: 'assemble',
+  staticUrl: 'https://rink.hockeyapp.net/api/2/apps/upload',
+  pid: 0,
+  actions: {
+    build: function() {
+      this.set('is_loading', true);
+      let params = {};
+      params.cwd = this.get('path');
+      let command = this.get('type').command;
+      let buildTypes = [];
+      buildTypes.push(command);
+      let output = this.spawn("./gradlew", buildTypes, params);
+      this.set('pid', output.pid);
+      output.stdout.on('data', (data) => {
+        var _data = this.get('stdOut') + '\n' + data;
+        this.set('stdOut', _data);
+      });
 
-			output.stderr.on('data', (data) => {
-				this.set('stdErr', this.get('stdErr') + data);
-			});
+      output.stderr.on('data', (data) => {
+        this.set('stdErr', this.get('stdErr') + data);
+      });
 
-			output.on('close', (code) => {
-				this.set('stdEnd', "child process exited with code" + code);
-				this.set('is_loading', false);
-				this.set('success', (code === 0) );
-				this.set('error', !(code === 0) );
-			});
+      output.on('close', (code) => {
+        this.set('stdEnd', "child process exited with code" + code);
+        this.set('is_loading', false);
+        this.set('success', (code === 0));
+        this.set('error', !(code === 0));
+      });
 
-			output.on('error', (code) => {
-				this.set('stdEnd', "child process exited with code" + code);
-				this.set('success', (code === 0) );
-				this.set('error', !(code === 0) );
-				this.set('is_loading', false);
-			});
+      output.on('error', (code) => {
+        this.set('stdEnd', "child process exited with code" + code);
+        this.set('success', (code === 0));
+        this.set('error', !(code === 0));
+        this.set('is_loading', false);
+      });
 
-		},
+    },
 
-	}
+  }
 });
