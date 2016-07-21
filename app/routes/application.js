d0103fb12e18a62786247b3bc0ca7b3e0cf9575c
shrikanthkr <shrikanth_kr@yahoo.com>
2016-07-20 21:07:33 -0700

    push session management to sessions route rather than application route.

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,18 +1,5 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-	beforeModel(){
-		return this.store.findAll('user').then((users)=>{
-			let loggedIn = false;
-			let user = users.get('firstObject');
-			if(user){
-				this.controllerFor('application').set('currentUser', user);
-				this.transitionTo('/index');
-				return;
-			}else{
-				this.transitionTo('/');
-				return;
-			}
-		});
-	}
+	
 });
